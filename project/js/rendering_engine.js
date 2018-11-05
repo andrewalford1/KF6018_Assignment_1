385e02ce6ca313254e93cb18eb131e8e73eb8a8b
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2018-11-05 22:43:43 +0000

    Update project/js/rendering_engine.js
    Changed 'vars' to 'lets'.
    - Makes code safer as variables are now block specific and not function specific.

--- project/js/rendering_engine.js
+++ project/js/rendering_engine.js
@@ -1,12 +1,12 @@
 //MAIN CODE...
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 
 // Ambient light
-var lightAmbient = new THREE.AmbientLight(colours.WHITE, 0.2); // soft white light
+let lightAmbient = new THREE.AmbientLight(colours.WHITE, 0.2); // soft white light
 scene.add(lightAmbient);
 
 //point light.
-var pointLight = new THREE.PointLight(colours.WHITE, 1);
+let pointLight = new THREE.PointLight(colours.WHITE, 1);
 pointLight.position.set(25, 50, 25);
 pointLight.castShadow = true;
 pointLight.shadow.mapSize.width = 1024;
@@ -14,18 +14,18 @@ pointLight.shadow.mapSize.height = 1024;
 scene.add(pointLight);
 
 //Perspective projection parameters.
-var camera = new THREE.PerspectiveCamera(
+let camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
 
 //[controls] Used to orbit around the scene (just for debugging).
-var controls = new THREE.OrbitControls(camera);
+let controls = new THREE.OrbitControls(camera);
 
 //Set the initial position of the camera.
 camera.position.set(0, 0, 100);
 controls.update();
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 
 //Size of the 2D projection.
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -40,23 +40,23 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 //Connect the renderer to the canvas.
 document.body.appendChild(renderer.domElement);
 
-var stats = new Stats();
+let stats = new Stats();
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
 
 //ADD OBJECTS TO THE SCENE...
-for(i = 0; i < updateableObjects.length; i++)
+for(let i = 0; i < updateableObjects.length; i++)
 {
     updateableObjects[i].addToScene(scene);
 }
 
 //Timing variables...
 //[frameTime] The amount of time taken to compute and render a frame of animation in milliseconds.
-var frameTimeMs = 0;
+let frameTimeMs = 0;
 //[previousTime] The amount of time taken to compute and render the previous frame in milliseconds.
-var previousTimeMs = 0;
+let previousTimeMs = 0;
 //[currentTime] Stores the current time in milliseconds.
-var currentTimeMs = 0;
+let currentTimeMs = 0;
 
 //ANIMATION FUNCTION...
 function animate()
@@ -67,7 +67,7 @@ function animate()
 
     //Animation code...
     //Update all the updateable objects on the canvas.
-    for(i = 0; i < updateableObjects.length; i++)
+    for(let i = 0; i < updateableObjects.length; i++)
     {
         updateableObjects[i].update(frameTimeMs); 
     }
@@ -86,3 +86,4 @@ function animate()
 
 //Run the animation loop.
 animate();
+
