38184d33e9b1b99a60e5ce4c1a62a749a5678aba
andrew.alford <w16006135@northumbria.ac.uk>
2018-11-06 14:45:05 +0000

    -Re-sized the sun (now bigger) -Spread planets out further
    Signed-off-by: andrew.alford <w16006135@northumbria.ac.uk>

--- project/js/object_declerations.js
+++ project/js/object_declerations.js
@@ -1,13 +1,13 @@
 //CREATE OBJECTS...
 
 //Stars:
-const SUN = new Star(16, 2, 0.01, new THREE.Vector3(0, 0, 0));
+const SUN = new Star(32, 2, 0.01, new THREE.Vector3(0, 0, 0));
 
 //Planets:
-const NEW_BORN_PLANET = new NewBornPlanet(0.01, new THREE.Vector3(32, 0, 0), 0.5, SUN, 18500);
-const NATURE_PLANET = new NaturePlanet(0.01, new THREE.Vector3(48, 0, 0), 0.52, SUN, 12000);
-const CITY_PLANET = new CityPlanet(0.01, new THREE.Vector3(64, 0, 0), 0.54, SUN, 25400);
-const DYING_PLANET = new DyingPlanet(0.01, new THREE.Vector3(92, 0, 0), 0.56, SUN, 36500);
+const NEW_BORN_PLANET = new NewBornPlanet(0.01, new THREE.Vector3(62, 0, 0), 0.5, SUN, 18500);
+const NATURE_PLANET = new NaturePlanet(0.01, new THREE.Vector3(78, 0, 0), 0.52, SUN, 12000);
+const CITY_PLANET = new CityPlanet(0.01, new THREE.Vector3(100, 0, 0), 0.54, SUN, 25400);
+const DYING_PLANET = new DyingPlanet(0.01, new THREE.Vector3(124, 0, 0), 0.56, SUN, 36500);
 
 //Asteroids:
 const ASTEROID_B612 = new AsteroidB612(
