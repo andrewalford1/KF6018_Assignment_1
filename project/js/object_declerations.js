739a26f63e820fcbd88f7fb89e0a8f2ba1b50d38
andrewalford1 <andrewalford1@outlook.com>
2018-11-11 15:22:45 +0000

    Update project/js/object_declerations.js
    showed my parents

--- project/js/object_declerations.js
+++ project/js/object_declerations.js
@@ -4,7 +4,7 @@
 const SUN = new Star(32, 2, 0.01, new THREE.Vector3(0, 0, 0), "The sun is mass of incandescent gas, a gigantic nuclear furnace. \n Where hydrogen is built into helium at a temperature of millions of degrees", true);
 
 //Planets:
-const NEW_BORN_PLANET = new NewBornPlanet(0.01, new THREE.Vector3(62, 0, 0), 0.5, SUN, 18500, !(SUN.getSpinsClockwise()));
+const NEW_BORN_PLANET = new NewBornPlanet(0.01, new THREE.Vector3(62, 0, 0), 0.5, SUN, 185000, !(SUN.getSpinsClockwise()));
 const NATURE_PLANET = new NaturePlanet(0.01, new THREE.Vector3(78, 0, 0), 0.52, SUN, 12000, !(SUN.getSpinsClockwise()));
 const CITY_PLANET = new CityPlanet(0.01, new THREE.Vector3(100, 0, 0), 0.54, SUN, 25400, !(SUN.getSpinsClockwise()));
 const DYING_PLANET = new DyingPlanet(0.01, new THREE.Vector3(124, 0, 0), 0.56, SUN, 36500, !(SUN.getSpinsClockwise()));
@@ -38,4 +38,4 @@ for(let i = 0; i < UPDATEABLE_OBJECTS.length; i++)
     UPDATEABLE_OBJECTS[i].setActive(true); 
 }
 
-UPDATEABLE_OBJECTS[0].setInFocus(true);
+//UPDATEABLE_OBJECTS[0].setInFocus(true);
