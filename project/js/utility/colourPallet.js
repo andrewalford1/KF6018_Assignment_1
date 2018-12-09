83af4b5e4be435e0f79b6b1a64001723b384f314
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2018-12-09 23:01:02 +0000

    Update colourPallet.js
    Added sand colour

--- project/js/utility/colourPallet.js
+++ project/js/utility/colourPallet.js
@@ -17,7 +17,7 @@ const colours = {
     SIENNA:     0x8B4513,
     EARTH:      0x5A4D41,
     BROWN:      0x25201B,
+    SAND:       0xC2B280,
     PINK:       0xFF3361,
-    ORANGE:     0xFF963A,
-    SILVER:     0xCCCCCC
+    ORANGE:     0xFF963A
 };
